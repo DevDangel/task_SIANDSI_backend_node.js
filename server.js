105673d1482bbb8eb73ff8a5ec4100c9f0b3efdd
DevAngel <angeldavidh18@gmail.com>
2026-02-11 16:55:15 -0500

    config cors

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ['https://task-siandsi-frontend-react.vercel.app/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
